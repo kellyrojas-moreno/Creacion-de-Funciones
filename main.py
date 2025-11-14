133447f6bc699a8e9084aad0277902388a213db4
Kelly Rojas <kellyyanethrojasmoreno@gmail.com>
2025-11-14 14:48:18 -0500

    Ejercicio de funciones

--- main.py
+++ main.py
@@ -3,3 +3,31 @@ def main():
 
 if __name__=="__main__":
   main()
+
+
+  def addmultiplenumbersin(num1, num2, num3): 
+    suma = num1 + num2 +num3
+    return suma
+  print(addmultiplenumbersin(2,4,3))
+
+ 
+
+
+
+  #addmultiplenumbers([num, num, ..]) – esta función debe existir en
+  #tu programa, debe recibir una lista de números como entrada y 
+  #debe devolver la suma de esos números.
+
+  #multiplymultiplenumbers([num, num, ..]) – esta función debe 
+  #existir en tu programa, debe recibir una lista de números como
+  #entrada y debe devolver el resultado de multiplicar cada número
+  #sucesivamente con el siguiente.
+
+  #isiteven(num) – esta función debe existir en tu programa, debe
+  #recibir un solo número como entrada y debe devolver un valor
+  #booleano: True si el número es un número entero y par, False
+  #en caso contrario.
+
+  #isitaninteger(num) – esta función debe existir en tu programa, 
+  #debe recibir un solo número como entrada y debe devolver un valor
+  #booleano: True si el número es un entero, False en caso contrario.
